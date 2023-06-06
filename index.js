a1956af7a5e2e243203c5a573f25534e36d48f12
nimra-ali <nimrakhan098ali@gmail.com>
2023-06-06 00:01:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// function calculate(){
+//     var num1 = Number(prompt("enter your num1"));
+//     var num2 = Number(prompt("enter your num2"));
+//     // var opr = Number(prompt("enter your opr"));
+//     function cals(num1,num2){
+//           return num1+num2
+    
+
+//     }  
+//     console.log (cals(num1,num2));
+  
+    
+// }
+// calculate();
+
+// function calculater(){
+//     var num1 = Number(prompt("enter your num1"));
+//     var num2 = Number(prompt("enter your num2"));
+//     function cale(num1,num2){
+//           return num1-num2
+    
+
+//     }  
+//     console.log (cale(num1,num2));
+  
+    
+// }
+// calculater();
+
+// function calculaters(){
+//     var num1 = Number(prompt("enter your num1"));
+//     var num2 = Number(prompt("enter your num2"));
+//     // var opr = Number(prompt("enter your opr"));
+//     function calr(num1,num2){
+//           return num1/num2
+    
+
+//     }  
+//     console.log (calr(num1,num2));
+  
+    
+// }
+// calculaters();
+
+// function calculate(){
+//     var num1 = Number(prompt("enter your num1"));
+//     var num2 = Number(prompt("enter your num2"));
+//     // var opr = Number(prompt("enter your opr"));
+//     function cals(num1,num2){
+//           return num1+num2
+    
+
+//     }  
+//     console.log (cals(num1,num2));
+  
+    
+// }
+// calculate();
+
+
